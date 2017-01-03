c8a77ba3dc5e15eeda019a960b5dd2cae56de2df
suin <suinyeze@gmail.com>
2017-01-03 23:16:54 +0900

    Use shell script instead of Brewfile
    Signed-off-by: suin <suinyeze@gmail.com>

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+brew tap \
+	codekitchen/dinghy
+brew install \
+	ag \
+	curl \
+	dinghy \
+	docker \
+	docker-machine \
+	docker-compose \
+	git \
+	ngrok \
+	httpie \
+	hub \
+	node \
+	reattach-to-user-namespace \
+	pandoc \
+	stone \
+	tig \
+	tmux \
+	tree \
+	vim \
+	vimpager \
+	wget \
+	yarn \
+	zsh \
+	zsh-completions 
+
